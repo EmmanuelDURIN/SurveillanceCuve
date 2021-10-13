6c5a6e9a5d4b7a6e3f45122e8b92ada5d9bad40c
Emmanuel <emmanueldurin@gmail.com>
2021-10-13 16:07:02 +0200

    Création WindowCuve

--- SurveillanceCuve/MainWindow.xaml.vb
+++ SurveillanceCuve/MainWindow.xaml.vb
@@ -1,3 +1,10 @@
 ﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim window As New WindowCuve()
 
+        Dim result As Boolean? = window.ShowDialog()
+        If result = True Then
+            ' l'utilisateur a appuyé sur Ok
+        End If
+    End Sub
 End Class
---
+++ SurveillanceCuve/WindowCuve.xaml.vb
@@ -0,0 +1,9 @@
+﻿Public Class WindowCuve
+    Private Sub buttonOk_Click(sender As Object, e As RoutedEventArgs) Handles buttonOk.Click
+        Me.DialogResult = True
+    End Sub
+
+    Private Sub buttonCancel_Click(sender As Object, e As RoutedEventArgs) Handles buttonCancel.Click
+        Me.DialogResult = False
+    End Sub
+End Class
